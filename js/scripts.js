45e38783b20b6672d9d4857acf2e827654319da8
LuisaFermino <souzafermino49@gmail.com>
2023-10-26 08:39:47 -0300

    Implementação de dinamismo no menu lateral

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,16 @@ function logar() {
     alert("Usuário ou senha incorretos");
   }
 }
+
+function acionarMenu() {
+  const acionarmenu = document.querySelector(".container-menu");
+  if (acionarmenu !== null) {
+    acionarmenu.classList.remove("escondido");
+  }
+}
+
+function fecharMenu() {
+  const fecharMenu = document.querySelector(".container-menu");
+
+  fecharMenu.classList.add("escondido");
+}
