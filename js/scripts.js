09c60446511ed24bd31a16bf2f421b39f948d6b6
LuisaFermino <souzafermino49@gmail.com>
2023-10-27 14:22:30 -0300

    Comentários para próximos passos e teste de inserção de mensagens

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,8 @@
 const acionarmenu = document.querySelector(".container-menu");
+let mensagemNormais;
+let mensagemStatus;
+let mensagemPrivada;
+let mensagemEscolhida;
 const usuarios = [
   {
     pessoa: "João",
@@ -6,6 +10,13 @@ const usuarios = [
     text: "entra na sala...",
     type: "status",
     time: "08:01:17",
+  },
+  {
+    from: "João",
+    to: "Todos",
+    text: "Bom dia",
+    type: "message",
+    time: "08:02:50",
   },
   {
     from: "João",
@@ -38,11 +49,33 @@ function fecharMenu() {
 function enviarMensagem() {
   const mensagemDigitada = document.querySelector(".mensagem").value;
   const containerMensagens = document.querySelector(".container-mensagens");
-  containerMensagens.innerHTML += `<div class="mensagem-status">
-  <p class="textos">
-    <span class="horario">(10:43)</span>
-    <span class="nome">${usuarios.pessoa}</span>
-    ${mensagemDigitada}
-  </p>
-</div>`;
+  if (mensagemEscolhida === mensagemNormais) {
+    containerMensagens.innerHTML += `<div class="mensagem-normais">
+    <p class="textos">
+      <span class="horario">(10:43)</span>
+      <span class="nome">João</span>
+      entra na sala...
+    </p>
+  </div>`;
+  } else if (mensagemEscolhida === mensagemStatus) {
+    containerMensagens.innerHTML += `<div class="mensagem-status">
+    <p class="textos">
+      <span class="horario">(10:43)</span>
+      <span class="nome">João</span>
+      entra na sala...
+    </p>
+  </div>`;
+  } else if (mensagemEscolhida === mensagemPrivada) {
+    containerMensagens.innerHTML += `<div class="mensagem-reservada">
+    <p class="textos">
+      <span class="horario">(10:43)</span>
+      <span class="nome">João</span>
+      entra na sala...
+    </p> 
+  </div>`;
+  }
 }
+
+//identificar o tipo da mensagem atrasves do type que esta no objeto
+//Fazer um for para conseguir lançar as mensagens e utilizar o objeto para preenche-las
+//Permitir ao usuario que coloque qual tipo de mensagem e para quem (marcar com check)
