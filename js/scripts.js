ef5d17d3340472d28e91621146bb572025376ea9
LuisaFermino <souzafermino49@gmail.com>
2023-11-09 22:49:31 -0300

    Correções para envio de mensagem, manter conexão e scroll

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,93 @@
 const acionarmenu = document.querySelector(".container-menu");
+const resposta = {
+  data: [
+    {
+      from: "alo",
+      to: "socorro",
+      text: "stevan ferreira",
+      type: "message",
+      time: "20:07",
+    },
+    {
+      from: "alo",
+      to: "socorro",
+      text: "stevan ferreira",
+      type: "message",
+      time: "20:07",
+    },
+    {
+      from: "alo",
+      to: "socorro",
+      text: "stevan ferreira",
+      type: "message",
+      time: "20:07",
+    },
+    {
+      from: "alo",
+      to: "socorro",
+      text: "stevan ferreira",
+      type: "message",
+      time: "20:07",
+    },
+    {
+      from: "alo",
+      to: "socorro",
+      text: "stevan ferreira",
+      type: "message",
+      time: "20:07",
+    },
+    {
+      from: "alo",
+      to: "socorro",
+      text: "stevan ferreira",
+      type: "message",
+      time: "20:07",
+    },
+    {
+      from: "alo",
+      to: "socorro",
+      text: "stevan ferreira",
+      type: "message",
+      time: "20:07",
+    },
+    {
+      from: "alo",
+      to: "socorro",
+      text: "stevan ferreira",
+      type: "message",
+      time: "20:07",
+    },
+    {
+      from: "alo",
+      to: "socorro",
+      text: "stevan ferreira",
+      type: "message",
+      time: "20:07",
+    },
+    {
+      from: "alo",
+      to: "socorro",
+      text: "stevan ferreira",
+      type: "message",
+      time: "20:07",
+    },
+    {
+      from: "alo",
+      to: "socorro",
+      text: "stevan ferreira",
+      type: "message",
+      time: "20:07",
+    },
+    {
+      from: "alo",
+      to: "socorro",
+      text: "stevan ferreira",
+      type: "message",
+      time: "20:07",
+    },
+  ],
+};
+let nomeUsuario = "";
 
 let destinatario = "Todos";
 const usuariosAtivos = [
@@ -19,19 +108,21 @@ function logar() {
 
   nomeUsuario = document.querySelector(".entrada").value;
 
-  const requisicao = axios.post("http://localhost:5000/participants", {
-    name: nomeUsuario,
-  });
-
-  requisicao.then((resp) => {
-    acionarHome.classList.remove("escondido");
-    escondeTelaInicial.classList.add("escondido");
-  });
-  requisicao.catch((error) => {
-    alert("Usuário já existe");
-  });
+  axios
+    .post("http://localhost:5000/participants", {
+      name: nomeUsuario,
+    })
+    .then((resp) => {
+      acionarHome.classList.remove("escondido");
+      escondeTelaInicial.classList.add("escondido");
+      setInterval(manterConexaoUsuario, 5000);
+    })
+    .catch((error) => {
+      alert("Usuário já existe");
+    });
 
   usuariosNaTela();
+  atualizaMensagens();
 }
 
 function acionarMenu() {
@@ -55,7 +146,7 @@ function enviarMensagem() {
     type: "message",
   };
 
-  const mensagem = axios.post("http://localhost:5000/messages", objetoMensagem);
+  axios.post("http://localhost:5000/messages", objetoMensagem);
 
   containerMensagens.innerHTML = "";
   mensagemDigitada.value = "";
@@ -64,21 +155,25 @@ function enviarMensagem() {
 
 //manter a requisição para mensagens atualizadas
 function atualizaMensagens() {
-  const mensagens = axios.get("http://localhost:5000/messages");
-  mensagens.then(mensagensNaTela);
-  mensagens.catch(() => {
-    console.log("Deu erro");
-  });
+  // const mensagens = axios.get("http://localhost:5000/messages");
+  // mensagens.then(mensagensNaTela);
+  // mensagens.catch(() => {
+  //   console.log("Deu erro");
+  // });
+  //esta comentado pq passei um objeto na MARRA
+
+  mensagensNaTela(resposta);
 }
 
-setInterval(atualizaMensagens, 5000);
+// setInterval(atualizaMensagens, 5000);
 
 function mensagensNaTela(resposta) {
+  console.log(resposta);
   const containerMensagens = document.querySelector(".container-mensagens");
 
   for (let i = 0; i < resposta.data.length; i++) {
     if (resposta.data[i].type === "message") {
-      containerMensagens.innerHTML += `<div class="mensagem-normais">
+      containerMensagens.innerHTML += `<div class="mensagem-normais mensagens">
       <p class="textos">
         <span class="horario">(${resposta.data[i].time})</span>
         <span class="nome"> ${resposta.data[i].from} </spa n> para <span class="para-quem">${resposta.data[i].to}:</span>
@@ -86,7 +181,7 @@ function mensagensNaTela(resposta) {
       </p>
     </div>`;
     } else if (resposta.data[i].type === "status") {
-      containerMensagens.innerHTML += `<div class="mensagem-status">
+      containerMensagens.innerHTML += `<div class="mensagem-status mensagens">
       <p class="textos">
        <span class="horario">(${resposta.data[i].time})</span>  
         <span class="nome">${resposta.data[i].from}</span>
@@ -97,7 +192,7 @@ function mensagensNaTela(resposta) {
       resposta.data[i].type === "private" &&
       resposta.data[i].to === nomeUsuario
     ) {
-      containerMensagens.innerHTML += `<div class="mensagem-reservada">
+      containerMensagens.innerHTML += `<div class="mensagem-reservada mensagens">
       <p class="textos">
       <span class="horario">(${resposta.data[i].time})</span>
       <span class="nome">${resposta.data[i].from}</span> reservadamente para <span class="para-quem">${resposta.data[i].to}:</span>
@@ -106,13 +201,17 @@ function mensagensNaTela(resposta) {
     </div>`;
     }
   }
+  const scrollMensagens = document.querySelectorAll(".mensagens");
+  const ultimaMensagem = scrollMensagens[scrollMensagens.length - 1];
+  ultimaMensagem.scrollIntoView();
 }
 
 //manter a conexão do usuário depois de logado
-// function manterConexaoUsuario() {
-//   const usuarioAtivo = axios.post("http://localhost:5000/status", usuario);
-// }
-// setInterval(manterConexaoUsuario, 5000);
+function manterConexaoUsuario() {
+  const usuarioAtivo = axios.post("http://localhost:5000/status", {
+    name: nomeUsuario,
+  });
+}
 
 function usuariosNaTela() {
   const menuLateral = document.querySelector(".usuarios-ativos");
